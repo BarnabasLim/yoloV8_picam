572ce2957fa78498ffe4bec7e7e58d1d5f52e954
BarnabasLim <blim048@e.ntu.edu.sg>
2024-05-18 16:25:14 +0800

    [Build] added setup script

---
+++ yoloV8_picam/scripts/setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#test script
+echo Hello world lets setup
+
+cd ~/Desktop/code_text
+
+#Make required dir for imagtes
+mkdir -p ~/Desktop/code_test/captured_vehicle
+#Make scripts executable
+chmod +x -R ~/Desktop/code_test/yoloV8_picam/scripts/WORKING/
+#Move scripts to desktop
+cp ~/Desktop/code_test/yoloV8_picam/scripts/WORKING/* ~/Desktop/.
+
+#Create enviroment with installed packages from the system Python
+python -m venv --system-site-packages venv
+#Activate python enviroment
+source venv/bin/activate
+#Install requirements
+pip install -r ./yoloV8_picam/requirements.txt 
+
